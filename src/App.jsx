41b5f716262a9bfa130fb4be61254a9e5b8f6ac5
Ebenezer Noble <ebenezernoblest@gmail.com>
2025-06-02 08:15:39 +0100

    First release

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Layout from "./components/Layout"
+function App() {
+
+
+  return (
+     <Layout>
+        <h1 className="text-red-600">Hello</h1>
+     </Layout>
+  )
+}
+
+export default App
