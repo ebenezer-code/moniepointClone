f0c82f896016e9b39085adab04d5f91e06aeb8a6
Ebenezer Noble <ebenezernoblest@gmail.com>
2025-06-03 08:49:06 +0100

    Fix arrow icon

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -28,8 +28,6 @@ function Menu() {
             className="cursor-pointer relative transition-all ease-in-out duration-200"
           >
             {menu.name}
-            <MdKeyboardArrowDown />
-
             {activeMenu === i && (
               <div className="absolute top-full left-1/2 -translate-x-1/2 bg-red-600 text-white shadow-lg rounded-md p-4 z-50 flex gap-8 transition-all duration-300 min-w-[400px]">
                 <div className="w-1/2">
@@ -70,6 +68,7 @@ function Menu() {
             )}
           </li>
         ))}
+        <MdKeyboardArrowDown />
       </ul>
     </div>
   );
