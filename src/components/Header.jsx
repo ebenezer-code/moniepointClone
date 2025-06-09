a9f5c95e0cc99807ba35785dbf20ec46a1ae13ee
Ebenezer Noble <ebenezernoblest@gmail.com>
2025-06-09 17:26:52 +0100

    Fix screen size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,15 @@ const Header = () => {
   });
   return (
     <header className="w-full bg-[#061435] text-white sticky top-0 z-50 font-[Inter] text-[15px]">
-      <nav className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-start md:items-center justify-between h-auto md:h-20 p-0 lg:p-6 space-y-4 md:space-y-0 md:space-x-40">
-        <div className="flex flex-col-reverse md:flex-row md:items-center md:space-x-6 md:w-1/2 w-full">
-          <div className="flex w-full justify-between p-4 items-center md:w-auto md:p-0">
+      <nav className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-start lg:items-center justify-between h-auto lg:h-20 p-0 lg:p-6 space-y-4 lg:space-y-0 lg:space-x-40">
+        <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:space-x-6 lg:w-1/2 w-full">
+          <div className="flex w-full justify-between p-4 items-center lg:w-auto lg:p-0">
             <img
               src={logo}
               alt="Logo"
               className="h-7 hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"
             />
-            <div className="relative w-6 h-6 md:hidden">
+            <div className="relative w-6 h-6 lg:hidden">
               <IoMenu
                 onClick={() => setState({ ...state, isMenuOpen: true })}
                 className={`absolute top-0 left-0 text-2xl transition-opacity duration-200 ${
@@ -37,14 +37,14 @@ const Header = () => {
               />
             </div>
           </div>
-          <ul className="flex w-full h-14 md:h-8 md:flex md:space-x-6 md:w-auto md:border-none border-[#808080] border-b-[0.2px] cursor-pointer">
+          <ul className="flex w-full h-14 lg:h-8 lg:flex lg:space-x-6 lg:w-auto lg:border-none border-[#808080] border-b-[0.2px] cursor-pointer">
             <li
               onClick={() => setState({ ...state, isActive: "Business" })}
               className={`${
                 state.isActive === "Business"
-                  ? "bg-[#0E2256] md:bg-[#37435D]"
+                  ? "bg-[#0E2256] lg:bg-[#37435D]"
                   : "bg-transparent"
-              } md:rounded-full md:p-4 w-1/2 flex items-center justify-center md:w-auto md:py-0 md:border-none border-[#808080] border-r-[0.2px] transition-background duration-300 ease-in-out`}
+              } lg:rounded-full lg:p-4 w-1/2 flex items-center justify-center lg:w-auto lg:py-0 lg:border-none border-[#808080] border-r-[0.2px] transition-background duration-300 ease-in-out`}
             >
               Business
             </li>
@@ -52,27 +52,27 @@ const Header = () => {
               onClick={() => setState({ ...state, isActive: "Personal" })}
               className={`${
                 state.isActive === "Personal"
-                  ? "bg-[#0E2256] md:bg-[#37435D]"
+                  ? "bg-[#0E2256] lg:bg-[#37435D]"
                   : "bg-transparent"
-              }  md:rounded-full md:p-4 w-1/2 flex items-center justify-center md:w-auto md:py-0 md:border-none transition-background duration-300 ease-in-out`}
+              }  lg:rounded-full lg:p-4 w-1/2 flex items-center justify-center lg:w-auto lg:py-0 lg:border-none transition-background duration-300 ease-in-out`}
             >
               Personal
             </li>
           </ul>
         </div>
-        <div className="flex flex-col md:flex-row md:items-center md:space-x-6 md:w-1/2 w-full">
-          <div className="hidden md:flex space-x-6">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-6 lg:w-1/2 w-full">
+          <div className="hidden lg:flex space-x-6">
             <button>Products</button>
             <button>Company</button>
           </div>
-          <div className="hidden md:flex space-x-6">
+          <div className="hidden lg:flex space-x-6">
             <ul className="flex space-x-4">
               <li>About</li>
               <li>Contact</li>
               <li>Blog</li>
             </ul>
           </div>
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden lg:flex items-center space-x-4">
             <button>Sign in →</button>
             <div className="flex items-center space-x-1">
               <p>country logo</p>
